335a572cf2d6cfc45190ab2bc13156d129c7a6f9
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-09-05 14:08:15 -0500

    create Subscription

---
+++ db/migrate/20220905190721_create_subscriptions.rb
@@ -0,0 +1,11 @@
+class CreateSubscriptions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :subscriptions do |t|
+      t.integer :user_id
+      t.integer :product_id
+      t.string :frequency
+
+      t.timestamps
+    end
+  end
+end

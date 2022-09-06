0d8310e26377afa126a4b50eab1d176684376956
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-09-06 14:12:45 -0500

    build product controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::ProductsController < ApplicationController
+
+    def index 
+        @products = Product.all
+    end
+
+    def show
+        @product = Product.find(params[:id])
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      product = Product.where(params[:id])
+    end
+
+    def set_user
+      user = User.find_by(params[:user_id]) 
+    end
+
+    # Only allow a list of trusted parameters through.
+    def product_params
+      params.require(:product).permit(:name, :brand, :product_type)
+    end
 end

20ad3d6f2dddb6dfe6b0851c787e4173a1e6e418
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-09-05 20:21:50 -0500

    create relationships and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :subscriptions
+    has_many :products, through: :subscriptions
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+    has_secure_password
 end

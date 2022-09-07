b2a5318397b3c139ce12fc60fdc2f476be755f5f
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-09-07 11:12:33 -0500

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :products do 
-        resources :subscriptions 
-      end
+      resources :products, only: [:index, :show] 
+      resources :subscriptions 
       resources :users, only: [:create]
       post '/login', to: 'users#login'
       get "/auto_login", to: "users#auto_login"

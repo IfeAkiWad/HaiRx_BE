f95190188ac112a15e9fea4e077774870314f5a7
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-09-07 13:40:50 -0500

    remove password column from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_05_190721) do
+ActiveRecord::Schema.define(version: 2022_09_07_183930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 2022_09_05_190721) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "uid"
-    t.text "password"
   end
 
 end

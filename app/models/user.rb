ac7b1215521c554211a4115775cb90b797d91ff8
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-09-08 14:37:27 -0500

    install gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ApplicationRecord
     validates :password, presence: true
     accepts_nested_attributes_for :subscriptions
     has_secure_password
+
+    def self.from_omniauth(auth)
+        find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['first_name']
+            u.email = auth['info']['email']
+            # u.name = auth['info']['name']
+            u.password = SecureRandom.hex(15)
+        end
+    end
 end
 
 

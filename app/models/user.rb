18faa24b6f7daf0f0715600f8cd67dc699c89e63
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-09-25 16:46:07 -0500

    remove self method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
     accepts_nested_attributes_for :subscriptions
     has_secure_password
 
-    def self.from_omniauth(auth)
-        find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
-            u.username = auth['info']['first_name']
-            u.email = auth['info']['email']
-            # u.name = auth['info']['name']
-            u.password = SecureRandom.hex(15)
-        end
-    end
+    # def self.from_omniauth(auth)
+    #     find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+    #         u.username = auth['info']['first_name']
+    #         u.email = auth['info']['email']
+    #         # u.name = auth['info']['name']
+    #         u.password = SecureRandom.hex(15)
+    #     end
+    # end
 end
 
 

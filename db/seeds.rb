51ca99c81c5631593e35807491ca0a9fc5d84915
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-01-11 11:23:47 -0600

    refactor dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,11 +26,11 @@ Product.create(name: "Rosemary Mint Strengthening Hair Masque", brand: "Mielle O
 Product.create(name: "B. Well Organic + Australian 100% Tea Tree Oil", brand: "Briogeo", product_type: "Oil")
 Product.create(name: "Coconut Oil Styling Cream", brand: "Beleza Natural", product_type: "Curl Cream")
 
-User.create(username: "LillyPop",  password_digest: "flowers", name: "Lillian Perez" , email: "lilly@perez.com")
-User.create(username: "alleygator",  password_digest: "john1122son", name: "Allison Johnson", email: "Alley@johnson.com")
-User.create(username: "swadewade",  password_digest: "curvacious" , name: "Matthew Wade", email: "matthew@wade.com")
-User.create(username: "ToyToy",  password_digest: "Akin94" , name: "Toyin Estrada", email: "Toyin@Estrada.com" )
-User.create(username: "RomanWWE",  password_digest: "GOAT" , name: "Roman Reigns", email: "roman@reigns.com" )
+User.create(password_digest: "flowers", email: "lilly@perez.com")
+User.create(password_digest: "john1122son", email: "Alley@johnson.com")
+User.create(password_digest: "curvacious" , email: "matthew@wade.com")
+User.create(password_digest: "Akin94" , email: "Toyin@Estrada.com" )
+User.create(password_digest: "GOAT" , email: "roman@reigns.com" )
 
 User.all.each do |user|
     frequencies = ["bi-weekly", "monthly", "bi-monthly"]

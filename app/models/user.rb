c4c69045934a9b561b2bdd6fa52053c305320793
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-01-11 11:14:16 -0600

    refactor User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_many :subscriptions
     has_many :products, through: :subscriptions
-    validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
     accepts_nested_attributes_for :subscriptions

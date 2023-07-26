9c301e5d01f2666c490393ea03ce180b2ef5cae2
Ifeoluwa Akinremi-Wade <ifeoluwa.wade@gmail.com>
2023-07-26 14:23:22 -0500

    remove password attribute from params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -47,6 +47,6 @@ class Api::V1::UsersController < ApplicationController
       end
  
      def user_params
-       params.require(:user).permit( :password, :email)
+       params.require(:user).permit(:email)
      end
 end
